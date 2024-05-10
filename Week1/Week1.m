217b73583af5e78664815005e7be0be3bc758c45
Fatemeh Jamshidian <96016047+fatemehjamshidian@users.noreply.github.com>
2024-05-10 10:34:28 -0400

    Create Week1.m

---
+++ Week1/Week1.m
@@ -0,0 +1,19 @@
+% Q 1
+A = randn(6,8)
+
+% Q 2
+a= false(4)
+b= zeros(4)
+
+% Q 3
+A= rand(4,5)
+A_transpose= A'
+
+% Q 4
+m=[9 2 6 ;23 3 12 ; 6 6 16]
+n=[5 22 5 ;26 59 16 ; 24 0 16]
+
+simple=m.*n
+inner_p=m*n
+
+mod_m_n= mod(m,n)
